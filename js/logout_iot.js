d1a39b99be810e953d07e0811cb10a5171ff348b
Jesus Caballero <62200154+jdcaballeroco@users.noreply.github.com>
2025-10-11 11:34:04 -0500

    [11-10-2025]-2

--- js/logout_iot.js
+++ js/logout_iot.js
@@ -7,22 +7,6 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleMenus = document.querySelectorAll('.toggle-menu'); // Seleccionamos todos los botones de expansión
-    
-    toggleMenus.forEach(menu => {
-        menu.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevenimos la acción predeterminada del enlace
-            const submenu = this.nextElementSibling; // El siguiente elemento es el submenú
-            const icon = this.querySelector('.toggle-icon'); // El icono de expansión
-
-            // Alternar la clase "active" para mostrar u ocultar el submenú
-            submenu.classList.add('active');
-            icon.classList.add('active'); // Cambiar el icono (rotación)
-        });
-    });
-});
-
 // Función para cerrar sesión
 function cerrarSesion() {
     sessionStorage.removeItem("logueado-iot");  // Eliminar la sesión
